ba53ec05ca6586537e059be019af7847fe17a5d9
Jared Reisinger <jaredreisinger@hotmail.com>
2026-04-02 21:35:55 -0700

    refactor: migrate `committed hook ...` to just `committed ...`

--- cmd/hook.go
+++ cmd/hook.go
@@ -1,14 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/jaredreisinger/committed/internal/config"
-	"github.com/jaredreisinger/committed/internal/hook"
-	"github.com/jaredreisinger/committed/pkg/commit"
-	"github.com/jaredreisinger/committed/pkg/tui"
 	"github.com/spf13/cobra"
 )
 
@@ -16,81 +8,5 @@ var hookCmd = &cobra.Command{
 	Use:   "hook [message-file] [source] [sha1]",
 	Short: "Run as prepare-commit-msg hook",
 	Args:  cobra.RangeArgs(1, 3),
-	Run: func(cmd *cobra.Command, args []string) {
-		// Parse hook arguments
-		hookArgs := make([]string, len(os.Args))
-		copy(hookArgs, os.Args)
-		// Replace the subcommand with the program name for ParseHookArgs
-		hookArgs[1] = hookArgs[0] // program name
-		hookArgs = hookArgs[1:]   // remove the "hook" subcommand
-
-		hookCtx, err := hook.ParseHookArgs(hookArgs)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error parsing hook arguments: %v\n", err)
-			os.Exit(1)
-		}
-
-		// Load configuration
-		cfg, err := config.LoadConfig(".")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error loading config: %v\n", err)
-			os.Exit(1)
-		}
-
-		// Read existing commit message
-		existingContent, err := hook.ReadMessageFile(hookCtx.MessageFilePath)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading commit message: %v\n", err)
-			os.Exit(1)
-		}
-
-		// Parse existing message if present
-		var existingMsg *commit.Message
-		if existingContent != "" {
-			parsed, err := commit.ParseMessage(existingContent)
-			if err != nil {
-				// If parsing fails, treat as plain text in details field
-				existingMsg = &commit.Message{
-					Description: "",
-					Body:        existingContent,
-				}
-			} else {
-				existingMsg = parsed
-			}
-		}
-
-		// Run TUI
-		model := tui.NewModel(cfg, existingMsg)
-		p := tea.NewProgram(model)
-
-		finalModel, err := p.Run()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error running TUI: %v\n", err)
-			os.Exit(1)
-		}
-
-		tuiModel, ok := finalModel.(tui.Model)
-		if !ok {
-			fmt.Fprintf(os.Stderr, "Unexpected model type\n")
-			os.Exit(1)
-		}
-
-		// Get the result
-		resultMsg, err := tuiModel.Result()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Form not completed: %v\n", err)
-			os.Exit(1)
-		}
-
-		// Format and write the message
-		formatted := resultMsg.String()
-		err = hook.WriteMessageFile(hookCtx.MessageFilePath, formatted)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing commit message: %v\n", err)
-			os.Exit(1)
-		}
-
-		// Success
-		os.Exit(0)
-	},
+	Run:   runHook,
 }
--- cmd/root.go
+++ cmd/root.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/jaredreisinger/committed/internal/config"
+	"github.com/jaredreisinger/committed/internal/hook"
+	"github.com/jaredreisinger/committed/pkg/commit"
+	"github.com/jaredreisinger/committed/pkg/tui"
 	"github.com/spf13/cobra"
 )
 
 var version = "0.1.0"
 
 var rootCmd = &cobra.Command{
-	Use:   "committed",
+	Use:   "committed [message-file] [source] [sha1]",
 	Short: "Interactive conventional-commit assistant",
 	Long:  "A Go TUI that helps format conventional commit messages and can integrate with git hooks.",
+	Args:  cobra.RangeArgs(1, 3),
+	Run:   runHook,
 }
 
 func Execute() {
@@ -22,8 +29,78 @@ func Execute() {
 	}
 }
 
+func runHook(cmd *cobra.Command, args []string) {
+	// Build hook args matching hook.ParseHookArgs expectations: [program, message-file, source?, sha1?]
+	hookArgs := make([]string, len(os.Args))
+	copy(hookArgs, os.Args)
+
+	if cmd.Name() == "hook" {
+		// remove the "hook" subcommand argument for parse
+		if len(hookArgs) > 1 {
+			hookArgs = append(hookArgs[:1], hookArgs[2:]...)
+		}
+	}
+
+	hookCtx, err := hook.ParseHookArgs(hookArgs)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing hook arguments: %v\n", err)
+		os.Exit(1)
+	}
+
+	cfg, err := config.LoadConfig(".")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading config: %v\n", err)
+		os.Exit(1)
+	}
+
+	existingContent, err := hook.ReadMessageFile(hookCtx.MessageFilePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading commit message: %v\n", err)
+		os.Exit(1)
+	}
+
+	var existingMsg *commit.Message
+	if existingContent != "" {
+		parsed, err := commit.ParseMessage(existingContent)
+		if err != nil {
+			existingMsg = &commit.Message{Description: "", Body: existingContent}
+		} else {
+			existingMsg = parsed
+		}
+	}
+
+	model := tui.NewModel(cfg, existingMsg)
+	p := tea.NewProgram(model)
+
+	finalModel, err := p.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error running TUI: %v\n", err)
+		os.Exit(1)
+	}
+
+	tuiModel, ok := finalModel.(tui.Model)
+	if !ok {
+		fmt.Fprintf(os.Stderr, "Unexpected model type\n")
+		os.Exit(1)
+	}
+
+	resultMsg, err := tuiModel.Result()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Form not completed: %v\n", err)
+		os.Exit(1)
+	}
+
+	formatted := resultMsg.String()
+	err = hook.WriteMessageFile(hookCtx.MessageFilePath, formatted)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing commit message: %v\n", err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
 func init() {
-	rootCmd.AddCommand(hookCmd)
 	rootCmd.PersistentFlags().BoolP("version", "v", false, "Show version")
 	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
 		versionFlag, _ := cmd.Flags().GetBool("version")
