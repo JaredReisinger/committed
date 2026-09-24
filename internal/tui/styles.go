ac30041d9d0e2a582a0256488508ea524d74ec47
Jared Reisinger <jaredreisinger@hotmail.com>
2026-09-23 22:25:52 -0700

    feat: set up new color handling

--- internal/tui/styles.go
+++ internal/tui/styles.go
@@ -1,12 +1,94 @@
 package tui
 
 import (
+	"image/color"
+
 	"charm.land/lipgloss/v2"
 )
 
 // TODO: we can choose our colors and use lighten/darken to create the blurred
 // values.
 
+// We build styles up from basic colors, and then aggregate into a structure
+// that is particularly textinput/textarea-friendly. (We *could* decide to make
+// our textmodel wrapper handle that, which might make user-customization
+// easier.)
+
+// do we need a light/dark axis, too?
+
+type role int
+
+const (
+	foreground role = iota
+	// background
+	placeholder
+	decoration
+	cursor
+)
+
+type state int
+
+const (
+	blurred state = iota
+	focused
+)
+
+type status int
+
+const (
+	defaultStatus status = iota
+	invalid
+	valid
+)
+
+// colors
+var (
+	placeholderColor = lipgloss.Color("238")
+
+	defaultColors = map[status]map[state]map[role]color.Color{
+		defaultStatus: {
+			blurred: {
+				foreground:  lipgloss.Color("244"), // #808080
+				placeholder: placeholderColor,
+				decoration:  lipgloss.Color("238"), // #444444
+			},
+			focused: {
+				foreground:  lipgloss.Color("254"), // #e4e4e4
+				placeholder: placeholderColor,
+				decoration:  lipgloss.Color("27"), // #005fff
+				cursor:      lipgloss.Color("39"), // #00afff
+			},
+		},
+		invalid: {
+			blurred: {
+				foreground:  lipgloss.Color("167"), // #d75f5f
+				placeholder: placeholderColor,
+				decoration:  lipgloss.Color("88"), // #870000
+			},
+			focused: {
+				foreground:  lipgloss.Color("217"), // #ffafaf
+				placeholder: placeholderColor,
+				decoration:  lipgloss.Color("196"), // #ff0000
+				cursor:      lipgloss.Color("217"), // #ffafaf
+			},
+		},
+		valid: {
+			blurred: {
+				foreground:  lipgloss.Color("77"), // #5fd75f
+				placeholder: placeholderColor,
+				decoration:  lipgloss.Color("28"), // #008700
+			},
+			focused: {
+				foreground:  lipgloss.Color("157"), // #afffaf
+				placeholder: placeholderColor,
+				decoration:  lipgloss.Color("118"), // #87ff00
+				cursor:      lipgloss.Color("157"), // #afffaf
+			},
+		},
+	}
+)
+
+// decoration
 var (
 	underBorder = lipgloss.Border{
 		// Top:          " ",
@@ -28,26 +110,22 @@ var (
 
 	// focusedTextStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("250"))
 
-	decorationFocusColor = lipgloss.Color("201")
-	decorationBlurColor  = lipgloss.Color("238")
+	decorationFocusColor = defaultColors[defaultStatus][focused][decoration]
+	decorationBlurColor  = defaultColors[defaultStatus][blurred][decoration]
 
-	textPlaceholderColor = lipgloss.Color("238")
-	textBlurColor        = lipgloss.Color("244")
-	textFocusColor       = lipgloss.Color("250")
-
-	placeholderStyle = lipgloss.NewStyle().Foreground(textPlaceholderColor).Italic(true)
+	placeholderStyle = lipgloss.NewStyle().Foreground(defaultColors[defaultStatus][focused][placeholder]).Italic(true)
 
 	defaultTextStyles = textStyles{
 		Focused: textPartStyles{
-			Text:        lipgloss.NewStyle().Foreground(textFocusColor),
+			Text:        lipgloss.NewStyle().Foreground(defaultColors[defaultStatus][focused][foreground]),
 			Placeholder: placeholderStyle,
 		},
 		Blurred: textPartStyles{
-			Text:        lipgloss.NewStyle().Foreground(textBlurColor),
+			Text:        lipgloss.NewStyle().Foreground(defaultColors[defaultStatus][blurred][foreground]),
 			Placeholder: placeholderStyle,
 		},
 		Cursor: textCursorStyle{
-			Color: lipgloss.Color("147"),
+			Color: defaultColors[defaultStatus][focused][cursor],
 		},
 	}
 
